36524d8b2b0916165786e7e28a685dbb4021d659
Pablo Bassil <pabcubus@gmail.com>
2022-04-11 18:29:48 -0500

    first commit

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,28 +2,55 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Feed</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-card v-for="(item, index) in feed" :key="index">
+        <ion-card-header>
+          <ion-img :src="item.image" :alt="index"></ion-img>
+          <ion-card-subtitle>{{item.city}}</ion-card-subtitle>
+          <ion-card-title>{{item.type}}</ion-card-title>
+          <ion-button size="small" color="tertiary">
+            <ion-icon :icon="map" />
+          </ion-button>
+        </ion-card-header>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { map } from 'ionicons/icons';
 
 export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  name: 'Tab1Feed',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    return {
+      map
+    }
+  },
+  data: () => ({
+    feed: [
+      {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/A_surfer_surfing_on_the_ocean_%28Unsplash%29.jpg/640px-A_surfer_surfing_on_the_ocean_%28Unsplash%29.jpg',
+        city: 'Barranquilla, Colombia',
+        type: 'Hueco'
+      },
+      {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/A_surfer_surfing_on_the_ocean_%28Unsplash%29.jpg/640px-A_surfer_surfing_on_the_ocean_%28Unsplash%29.jpg',
+        city: 'Cartagena, Colombia',
+        type: 'Calle mala'
+      },
+      {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/A_surfer_surfing_on_the_ocean_%28Unsplash%29.jpg/640px-A_surfer_surfing_on_the_ocean_%28Unsplash%29.jpg',
+        city: 'Santa Marta, Colombia',
+        type: 'Hueco'
+      }
+    ]
+  })
 });
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,38 +3,38 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="feed" href="/tabs/tab1">
+          <ion-icon :icon="gridOutline" />
+          <ion-label>Feed</ion-label>
         </ion-tab-button>
           
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="new" href="/tabs/tab2">
+          <ion-icon :icon="addCircleOutline" />
+          <ion-label>Nuevo</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="settings" href="/tabs/tab3">
+          <ion-icon :icon="personCircleOutline" />
+          <ion-label>Settings</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { personCircleOutline, gridOutline, addCircleOutline } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      gridOutline,
+      personCircleOutline,
+      addCircleOutline
     }
   }
 });
